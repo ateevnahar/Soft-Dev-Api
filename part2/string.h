86a596550a0d86539689f1c8bc1b0a6298f35a4f
nahar <nahar.a@husky.neu.edu>
2020-01-28 16:30:50 -0500

    refined string.h to just have method stubs

--- part2/string.h
+++ part2/string.h
@@ -9,64 +9,33 @@ class String: public Object {
 		public:
     char* val_;
     size_t size_;
-		int count_;
-/*
+
   // constructor
   String(char* val) {
-    size_ = strlen(val); // lib func
-    val_ = new char[size_];
-    for(size_t i = 0; i < size_; i++) {
-      val_[i] = val[i];
-    }
   }
-*/
+
 	// constructor for const
   String(const char* val) {
-    size_ = strlen(val);  // lib func
-    val_ = new char[size_ + 1];
-    for(size_t i = 0; i < size_; i++) {
-      val_[i] = val[i];
-    }
-		val_[size_] = '\0';
-		count_ = 1;
   }
 
 	// destructor 
   ~String() {
-    delete[] val_;
   }
 
 	// hashing func (memoized)
   size_t hash_me() {
-    size_t res = 0;
-    for (size_t i = 0; i < size_; i++) {
-      res = res + val_[i];
-    }
-    return res;
   }
-//	void inc_count() {
-//	count_++;
-//	}
-	// equals func
-  bool equals(Object *o) {
-    if (o == nullptr) {
-      return false;
-    }
-    String *s = dynamic_cast<String*>(o);
-
-    if(s == nullptr) {
-      return false;
-    }
-
-    return strcmp(val_, s->val_) == 0;
+  
+	// used to compare strings
+	bool equals(Object *o) {
   }
 
+	// optional, use to print a string
+	void print() {
+	
+	}
+	// optional unneeded for this assignment
+	// but used to concat
 	String* concat(String* c) {
-		size_t concatSize = c->size_ + size_;
-		char* concated = new char[concatSize + 1];
-		strcpy(concated, val_);
-		strcat(concated, c->val_);
-		concated[concatSize] = '\0';
-		return new String(concated);
 	}
 };
