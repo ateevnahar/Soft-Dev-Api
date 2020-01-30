eb7c45e05aecb1f9f4d8b76ff17a4272cc4469cb
nahar <nahar.a@husky.neu.edu>
2020-01-30 16:43:40 -0500

    added children

--- part2/array.h
+++ part2/array.h
@@ -6,7 +6,6 @@
 class Array : public Object
 {
 public:
-
 	// constructor
 	Array(size_t size)
 	{
@@ -28,7 +27,7 @@ public:
 	}
 
 	// inserts given object at given index
-	void insert(* void to_add, size_t index)
+	void insert(*void to_add, size_t index)
 	{
 	}
 
@@ -36,7 +35,7 @@ public:
 	size_t len()
 	{
 	}
-	
+
 	// returns true if array is empty, false if it not
 	bool is_empty()
 	{
@@ -56,5 +55,160 @@ public:
 	void append(*void to_add)
 	{
 	}
+};
 
+class IntArray : public Array
+{
+public:
+	// constructor
+	IntArray(size_t size)
+	{
+	}
+
+	// destructor
+	~IntArray()
+	{
+	}
+
+	// inserts given object at given index
+	void insert(int *to_add, size_t index)
+	{
+	}
+
+	// returns pointer to object at given index
+	int *get(size_t index)
+	{
+	}
+
+	// prints particular element at given index
+	void print_index(size_t index)
+	{
+	}
+
+	// prints entire array
+	void print()
+	{
+	}
+
+	// append given object to the array if there is space
+	void append(int *to_add)
+	{
+	}
+};
+
+class BoolArray : public Array
+{
+public:
+	// constructor
+	BoolArray(size_t size)
+	{
+	}
+
+	// destructor
+	~BoolArray()
+	{
+	}
+
+	// inserts given object at given index
+	void insert(bool *to_add, size_t index)
+	{
+	}
+
+	// returns pointer to object at given index
+	bool *get(size_t index)
+	{
+	}
+
+	// prints particular element at given index
+	void print_index(size_t index)
+	{
+	}
+
+	// prints entire array
+	void print()
+	{
+	}
+
+	// append given object to the array if there is space
+	void append(bool *to_add)
+	{
+	}
+};
+
+class FloatArray : public Array
+{
+public:
+	// constructor
+	FloatArray(size_t size)
+	{
+	}
+
+	// destructor
+	~FloatArray()
+	{
+	}
+
+	// inserts given object at given index
+	void insert(float *to_add, size_t index)
+	{
+	}
+
+	// returns pointer to object at given index
+	float *get(size_t index)
+	{
+	}
+
+	// prints particular element at given index
+	void print_index(size_t index)
+	{
+	}
+
+	// prints entire array
+	void print()
+	{
+	}
+
+	// append given object to the array if there is space
+	void append(float *to_add)
+	{
+	}
+};
+
+class StringArray : public Array
+{
+public:
+	// constructor
+	StringArray(size_t size)
+	{
+	}
+
+	// destructor
+	~StringArray()
+	{
+	}
+
+	// inserts given object at given index
+	void insert(String *to_add, size_t index)
+	{
+	}
+
+	// returns pointer to object at given index
+	String *get(size_t index)
+	{
+	}
+
+	// prints particular element at given index
+	void print_index(size_t index)
+	{
+	}
+
+	// prints entire array
+	void print()
+	{
+	}
+
+	// append given object to the array if there is space
+	void append(String *to_add)
+	{
+	}
 };
